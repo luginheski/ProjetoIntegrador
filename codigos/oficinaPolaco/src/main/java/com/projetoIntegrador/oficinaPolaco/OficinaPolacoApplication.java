d511a9d34032cfaf85f2b9a1e613048ebe267ff5
luginheski <rodrigo514_2@hotmail.com>
2024-10-07 09:59:39 -0300

    Iniciando projeto
    Criando classes basicas

---
+++ codigos/oficinaPolaco/src/main/java/com/projetoIntegrador/oficinaPolaco/OficinaPolacoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador.oficinaPolaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaPolacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaPolacoApplication.class, args);
+	}
+
+}
---
+++ codigos/oficinaPolaco/src/test/java/com/projetoIntegrador/oficinaPolaco/OficinaPolacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador.oficinaPolaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaPolacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
