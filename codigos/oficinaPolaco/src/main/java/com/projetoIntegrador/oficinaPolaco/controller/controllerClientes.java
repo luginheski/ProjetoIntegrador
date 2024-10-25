fe20c92c008f8201c28aa3401d96c82a79f10c5f
luginheski <rodrigo514_2@hotmail.com>
2024-10-25 15:45:02 -0300

    upDate
    cadastro de clientes

--- codigos/oficinaPolaco/src/main/java/com/projetoIntegrador/oficinaPolaco/controller/controllerClientes.java
+++ codigos/oficinaPolaco/src/main/java/com/projetoIntegrador/oficinaPolaco/controller/controllerClientes.java
@@ -1,4 +1,3 @@
-
 package com.projetoIntegrador.oficinaPolaco.controller;
 
 import com.projetoIntegrador.oficinaPolaco.model.Cliente;
@@ -8,17 +7,58 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class controllerClientes {
-       @GetMapping("/cliente")
-    public String mostraCliente(){
+        
+    @GetMapping("/cliente")
+    public String mostraCliente() {
         return "cliente";
     }
     
-        @PostMapping("/cadastroCliente")
-    public String processarFilme(Model model, @ModelAttribute Cliente cliente) {
-        Dados.adicionarCliente(cliente);        
-        return "redirect:/listar";
+    @GetMapping("/inserir-cliente")
+    public String mostraCadastro(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "cliente";
+    }
+     
+    @GetMapping("/exibir")
+    public String mostraDetalhes(Model model, @RequestParam String id) {
+        Integer idCliente = Integer.parseInt(id);
+        Cliente clienteEncontrado = new Cliente();
+        clienteEncontrado = Dados.obtemCliente(idCliente);
+        model.addAttribute("clientes", clienteEncontrado);
+        return "cliente";
+    }    
+
+    @PostMapping("/cadastroCliente")
+    public String processarCliente(Model model, @ModelAttribute Cliente cliente) {
+        Dados.adicionarCliente(cliente);
+        
+        return "orcamentos";
+        
+ 
+    }   
+    
+     @GetMapping("/listagem")
+    public String mostraPiloto(Model model) {
+        model.addAttribute("clientes", Dados.listarClientes());
+        return "cliente";
+    }
+    
+       
+    @GetMapping("/excluir-cliente")
+    public String excluirPiloto(Model model, @RequestParam String id) {
+        Integer idCliente = Integer.parseInt(id);
+        Dados.excluirCliente(idCliente);
+        return "redirect:/listagem";
+    }
+    
+    @GetMapping("/alterar-cliente")
+    public String AlterarPiloto(Model model, @RequestParam String id) {
+        Integer idCliente = Integer.parseInt(id);
+        model.addAttribute("cliente", Dados.obtemCliente(idCliente));
+        return "cadastro";
     }
 }
--- codigos/oficinaPolaco/src/main/java/com/projetoIntegrador/oficinaPolaco/model/Dados.java
+++ codigos/oficinaPolaco/src/main/java/com/projetoIntegrador/oficinaPolaco/model/Dados.java
@@ -1,18 +1,54 @@
-
 package com.projetoIntegrador.oficinaPolaco.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Dados {
-    
-    private static List<Cliente> listaCliente = new ArrayList();
-    
 
-    
-    public static void adicionarCliente(Cliente cliente){
+    private static List<Cliente> listaCliente = new ArrayList();
+
+    public static void adicionarCliente(Cliente cliente) {
         cliente.setId(listaCliente.size() + 1);
         listaCliente.add(cliente);
-}
+    }
 
+    public static List<Cliente> listarClientes() {
+        return listaCliente;
+    }
+    
+    public static void excluirCliente(Integer id) {
+        for (Cliente c : listaCliente) {
+            if (c.getId() == id) {
+                listaCliente.remove(c);
+                break;
+            }
+        }
+    }
+
+    public static Cliente obtemCliente(Integer id) {
+        Cliente clienteEncontrado = new Cliente();
+        for (Cliente c : listaCliente) {
+            if (c.getId() == id) {
+                clienteEncontrado = c;
+                break;
+            }
+        }
+        return clienteEncontrado;
+    }
+
+    public static void atualizarPiloto(Cliente cliente) {
+        for (Cliente c : listaCliente) {
+            if (c.getId() == cliente.getId()) {
+                c.setNome(cliente.getNome());
+                c.setRg(cliente.getRg());
+                c.setCpf(cliente.getCpf());
+                c.setRua(cliente.getRua());
+                c.setNumero(cliente.getNumero());
+                c.setBairro(cliente.getBairro());
+                c.setCidade(cliente.getCidade());
+                c.setEstado(cliente.getEstado());
+                break;
+            }
+        }
+    }
 }
