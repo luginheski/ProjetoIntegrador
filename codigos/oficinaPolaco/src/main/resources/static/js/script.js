e277b71f529b114cc0ddad2b572c0ed478bb48e6
luginheski <rodrigo514_2@hotmail.com>
2024-11-21 22:50:13 -0300

    add
    validação

--- codigos/oficinaPolaco/src/main/resources/static/js/script.js
+++ codigos/oficinaPolaco/src/main/resources/static/js/script.js
@@ -17,3 +17,73 @@ function validaCpf(){
         return true;
     }
 }
+
+function validaTelefone(){
+    let telefone = document.getElementById("telefone").value;
+    if(telefone.length < 1){
+        alert("Informe um Telefone");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaRua(){
+    let rua = document.getElementById("rua").value;
+    if(rua.length < 1){
+        alert("Informe a rua");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaCep(){
+    let cep = document.getElementById("cep").value;
+    if(cep.length < 8){
+        alert("Informe um CEP");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaCidade(){
+    let cidade = document.getElementById("cidade").value;
+    if(cidade.length < 3){
+        alert("Informe uma cidade valida");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaNumero(){
+    let numero = document.getElementById("numero").value;
+    if(numero == 0){
+        alert("Informe o numero do endereço");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaBairro(){
+    let bairro = document.getElementById("bairro").value;
+    if(bairro < 3){
+        alert("Informe um bairro");
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaEstado(){
+    let estado = document.getElementById("estado").value;
+    if(estado < 2){
+        alert("Informe um estado");
+        return false;
+    }else{
+        return true;
+    }
+}
