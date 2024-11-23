8e32c78c45facb9e701ffa9e7c2b0354bd13f314
luginheski <rodrigo514_2@hotmail.com>
2024-11-22 23:11:22 -0300

    alterações
    alterado validações

--- codigos/oficinaPolaco/src/main/resources/static/js/script.js
+++ codigos/oficinaPolaco/src/main/resources/static/js/script.js
@@ -17,92 +17,22 @@ function calculaPeca(element){
     alert("valor total: "+ imprimir);
 }
 
-function validaNome(){
-    let nome = document.getElementById("nome").value;
-    if(nome.length < 3){
-        alert("Informe um nome válido");
-        return false;
-    }else{
-        return true;
-    }
-}
+var input = document.getElementById('nome');
+    input.oninvalid = function(event) {
+    event.target.setCustomValidity('O nome de usuário deve conter apenas letras no máximo com 50 caracteres. ex: Rodrigo');
+};
 
-function validaCpf(){
-    let cpf = document.getElementById("cpf").value;
-    if(cpf.length < 11){
-        alert("Informe CPF válido");
-        return false;
-    }else{
-        return true;
-    }
-}
+var input = document.getElementById('cpf');
+    input.oninvalid = function(event) {
+    event.target.setCustomValidity('O CPF deve conter apenas numeros no máximo com 11 caracteres. ex: 000.111.222-33');
+};
 
-function validaTelefone(){
-    let telefone = document.getElementById("telefone").value;
-    if(telefone.length < 1){
-        alert("Informe um Telefone");
-        return false;
-    }else{
-        return true;
-    }
-}
+var input = document.getElementById('rg');
+    input.oninvalid = function(event) {
+    event.target.setCustomValidity('O RG deve conter apenas números ');
+};
 
-function validaRua(){
-    let rua = document.getElementById("rua").value;
-    if(rua.length < 1){
-        alert("Informe a rua");
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function validaCep(){
-    let cep = document.getElementById("cep").value;
-    if(cep.length < 8){
-        alert("Informe um CEP");
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function validaCidade(){
-    let cidade = document.getElementById("cidade").value;
-    if(cidade.length < 3){
-        alert("Informe uma cidade valida");
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function validaNumero(){
-    let numero = document.getElementById("numero").value;
-    if(numero == 0){
-        alert("Informe o numero do endereço");
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function validaBairro(){
-    let bairro = document.getElementById("bairro").value;
-    if(bairro < 3){
-        alert("Informe um bairro");
-        return false;
-    }else{
-        return true;
-    }
-}
-
-function validaEstado(){
-    let estado = document.getElementById("estado").value;
-    if(estado < 2){
-        alert("Informe um estado");
-        return false;
-    }else{
-        return true;
-    }
-}
+var input = document.getElementById('telefone');
+    input.oninvalid = function(event) {
+    event.target.setCustomValidity('O telefone deve conter apenas numeros no máximo com 11 caracteres. ex: (41)99999-8888');
+};
